5b3dd81b2e25d4c4d4c08d3fb428ba3cc25cf831
ProyectoBaseD <jperezg43@miumg.edu.gt>
2022-03-30 19:18:05 -0600

    Se agrega el Proyecto

---
+++ EjercicioCotizacion/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace EjercicioCotizacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbnTotal = new System.Windows.Forms.RadioButton();
+            this.rbnTerceros = new System.Windows.Forms.RadioButton();
+            this.rbnBasico = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.chkAudio = new System.Windows.Forms.CheckBox();
+            this.chkAire = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCosto = new System.Windows.Forms.TextBox();
+            this.btnCotizar = new System.Windows.Forms.Button();
+            this.txtCotizacion = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(122, 25);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(156, 26);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(20, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 32);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Seguro";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbnTotal);
+            this.groupBox1.Controls.Add(this.rbnTerceros);
+            this.groupBox1.Controls.Add(this.rbnBasico);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(42, 102);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(249, 181);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Seguro a Seleccionar";
+            // 
+            // rbnTotal
+            // 
+            this.rbnTotal.AutoSize = true;
+            this.rbnTotal.Location = new System.Drawing.Point(124, 135);
+            this.rbnTotal.Name = "rbnTotal";
+            this.rbnTotal.Size = new System.Drawing.Size(69, 24);
+            this.rbnTotal.TabIndex = 5;
+            this.rbnTotal.Text = "Total";
+            this.rbnTotal.UseVisualStyleBackColor = true;
+            // 
+            // rbnTerceros
+            // 
+            this.rbnTerceros.AutoSize = true;
+            this.rbnTerceros.Location = new System.Drawing.Point(124, 95);
+            this.rbnTerceros.Name = "rbnTerceros";
+            this.rbnTerceros.Size = new System.Drawing.Size(107, 24);
+            this.rbnTerceros.TabIndex = 4;
+            this.rbnTerceros.Text = "A terceros";
+            this.rbnTerceros.UseVisualStyleBackColor = true;
+            // 
+            // rbnBasico
+            // 
+            this.rbnBasico.AutoSize = true;
+            this.rbnBasico.Checked = true;
+            this.rbnBasico.Location = new System.Drawing.Point(124, 52);
+            this.rbnBasico.Name = "rbnBasico";
+            this.rbnBasico.Size = new System.Drawing.Size(82, 24);
+            this.rbnBasico.TabIndex = 3;
+            this.rbnBasico.TabStop = true;
+            this.rbnBasico.Text = "Básico";
+            this.rbnBasico.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.chkAudio);
+            this.groupBox2.Controls.Add(this.chkAire);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(335, 102);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(294, 181);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Equipamiento";
+            // 
+            // chkAudio
+            // 
+            this.chkAudio.AutoSize = true;
+            this.chkAudio.Location = new System.Drawing.Point(117, 95);
+            this.chkAudio.Name = "chkAudio";
+            this.chkAudio.Size = new System.Drawing.Size(160, 24);
+            this.chkAudio.TabIndex = 7;
+            this.chkAudio.Text = "Sistema de Audio";
+            this.chkAudio.UseVisualStyleBackColor = true;
+            // 
+            // chkAire
+            // 
+            this.chkAire.AutoSize = true;
+            this.chkAire.Location = new System.Drawing.Point(117, 51);
+            this.chkAire.Name = "chkAire";
+            this.chkAire.Size = new System.Drawing.Size(172, 24);
+            this.chkAire.TabIndex = 6;
+            this.chkAire.Text = "Aire Acondicionado";
+            this.chkAire.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 32);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Equipo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(38, 315);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Costo";
+            // 
+            // txtCosto
+            // 
+            this.txtCosto.Location = new System.Drawing.Point(106, 315);
+            this.txtCosto.Name = "txtCosto";
+            this.txtCosto.Size = new System.Drawing.Size(168, 26);
+            this.txtCosto.TabIndex = 6;
+            // 
+            // btnCotizar
+            // 
+            this.btnCotizar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnCotizar.ForeColor = System.Drawing.Color.White;
+            this.btnCotizar.Location = new System.Drawing.Point(347, 308);
+            this.btnCotizar.Name = "btnCotizar";
+            this.btnCotizar.Size = new System.Drawing.Size(114, 40);
+            this.btnCotizar.TabIndex = 7;
+            this.btnCotizar.Text = "Cotizar";
+            this.btnCotizar.UseVisualStyleBackColor = false;
+            this.btnCotizar.Click += new System.EventHandler(this.btnCotizar_Click);
+            // 
+            // txtCotizacion
+            // 
+            this.txtCotizacion.Location = new System.Drawing.Point(42, 375);
+            this.txtCotizacion.Multiline = true;
+            this.txtCotizacion.Name = "txtCotizacion";
+            this.txtCotizacion.Size = new System.Drawing.Size(582, 189);
+            this.txtCotizacion.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 649);
+            this.Controls.Add(this.txtCotizacion);
+            this.Controls.Add(this.btnCotizar);
+            this.Controls.Add(this.txtCosto);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbnTotal;
+        private System.Windows.Forms.RadioButton rbnTerceros;
+        private System.Windows.Forms.RadioButton rbnBasico;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox chkAudio;
+        private System.Windows.Forms.CheckBox chkAire;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCosto;
+        private System.Windows.Forms.Button btnCotizar;
+        private System.Windows.Forms.TextBox txtCotizacion;
+    }
+}
+
---
+++ EjercicioCotizacion/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjercicioCotizacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCotizar_Click(object sender, EventArgs e)
+        {
+            double costo = 0.0;
+            string cotizacion = "";
+
+            cotizacion = $"Cotizació de Auto para {txtNombre.Text}\r\n ";
+            //obtener el costo inicial-
+            costo = Convert.ToDouble(txtCosto.Text);
+
+            //verificar seguros
+            if(rbnBasico.Checked==true)
+            {
+                costo = costo + 500.0;
+                cotizacion += "Llevar seguro básico de Q500.00 \r\n";
+            }
+            if(rbnTerceros.Checked==true)
+            {
+                costo += 700.0;
+                cotizacion = "Lleva seguro a terceros de Q700.00 \r\n";
+            }
+            if(rbnTotal.Checked==true)
+            {
+                costo += 1000.0;
+                cotizacion = "Lleva seguro Total de Q1000.00 \r\n";
+            }
+
+            //verificar equipo
+            if(chkAire.Checked==true)
+            {
+                costo +=500.0;
+                cotizacion += "Con aire acondicionado de Q500.00 \r\n";
+            }
+            if(chkAudio.Checked==true)
+            {
+                costo += 700.0;
+                cotizacion += "Con Sistema de Audio de Q700.00 \r\n";
+            }
+            //mostramos el total
+            cotizacion += "El total a pagar es de: "+costo.ToString();
+
+            txtCotizacion.Text = cotizacion;
+        }
+    }
+}
